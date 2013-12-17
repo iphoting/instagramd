ca6f87d5e8b1dc311e3ca257394415a93812931f
Ronald Ip <myself@iphoting.com>
2013-12-18 01:17:04 +0800

    Refactored.

--- instagramd.rb
+++ instagramd.rb
@@ -15,19 +15,35 @@ use Rack::Deflater
 use Rack::ConditionalGet
 
 get '/' do
-	@url = params[:url] unless params.nil?
-	haml :form
+	if params[:url]
+		begin
+			@url = URI::parse(params[:url])
+			@img_src = get_img_src(params[:url])
+			redirect to('/') if @img_src.nil?
+			haml :image
+		rescue URI::InvalidURIError
+			redirect to('/')
+		end
+	else
+		haml :form
+	end
 end
 
 post '/url' do
 	begin
 		@url = URI::parse(params[:url])
-		path = URI::split(params[:url])[5]
-		code_match = /\/p\/(?<code>\w+)\/?/.match(path)
-		redirect to('/') if code_match.nil?
-		@code = code_match[:code]
+		@img_src = get_img_src(params[:url])
+		redirect to('/') if @img_src.nil?
 		haml :image
 	rescue URI::InvalidURIError
 		redirect to('/')
 	end
+end
+
+def get_img_src(url)
+	path = URI::split(url)[5]
+	code_match = /\/p\/(?<code>\w+)\/?/.match(path)
+	return nil if code_match.nil?
+	code = code_match[:code]
+	return "https://instagram.com/p/#{code}/media/?size=l"
 end
